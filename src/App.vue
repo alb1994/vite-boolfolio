a69aea777531949b13e8f7faf283af344534d4fb
alb1994 <antonetti_alberto@libero.it>
2023-08-30 21:07:18 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import Appmain from './components/Appmain.vue';
+
+export default {
+  components: {
+    Appmain
+  }
+}
+</script>
+
+<template>
+  <div>
+    <Appmain />
+  </div>
+</template>
+
+<style lang="scss">
+@use './styles/generals.scss' as*;
+</style>
---
+++ src/components/Appmain.vue
@@ -0,0 +1,45 @@
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'AppMain',
+    data() {
+        return {
+            baseUrl: 'http://127.0.0.1:8000',
+            posts: [],
+            loading: true
+        };
+    },
+    created() {
+        this.getPosts();
+    },
+    methods: {
+        getPosts() {
+            this.loading = true;
+            axios.get(`${this.baseUrl}/api/posts`).then((response) => {
+                console.log(response);
+                if (response.data.success) {
+                    this.posts = response.data.results;
+                    this.loading = false;
+                } else {
+                    // Handle error here
+                }
+            });
+        }
+    }
+};
+</script>
+<template>
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    boolfolio
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+/* Add your styles here */
+</style>
