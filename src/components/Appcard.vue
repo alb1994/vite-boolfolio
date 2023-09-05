17abfd8397080fa9ae132a955ff577b6513db47a
alb1994 <antonetti_alberto@libero.it>
2023-09-05 21:50:36 +0200

    aggiunto Appcard e store.js

---
+++ src/components/Appcard.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="postcard">
+        <h2>{{ post.title }}</h2>
+        <p>{{ post.content }}</p>
+        <div v-if="post.tags && post.tags.length > 0">
+            <div v-for="tag in post.tags" :key="tag.id">
+                <h3>{{ tag.name }}</h3>
+            </div>
+        </div>
+        <div>
+            <span v-if="post.category">{{ post.category.name }}</span>
+            <span v-else>Categoria non assegnata</span>
+        </div>
+    </div>
+</template>
+  
+<script>
+import { store } from '../store.js';
+export default {
+    name: "Postcard",
+    props: {
+        post: Object,
+    },
+    data() {
+        return {
+            store,
+        }
+    },
+};
+</script>
+  
+<style scoped>
+.postcard {
+    border: 1px solid #ccc;
+    padding: 10px;
+    margin: 10px;
+}
+
+.postcard h2 {
+    margin: 0;
+    font-size: 18px;
+}
+
+.postcard h3 {
+    margin: 5px 0;
+    font-size: 14px;
+}
+
+.loading {
+    text-align: center;
+    padding: 20px;
+}
+</style>
--- src/pages/Postlist.vue
+++ src/pages/Postlist.vue
@@ -1,11 +1,16 @@
 <script>
 import axios from 'axios';
+import { store } from '../store.js';
+import Postcard from '../components/Appcard.vue';
 
 export default {
     name: 'Postlist',
+    components: {
+        Postcard
+    },
     data() {
         return {
-            baseUrl: 'http://localhost:8000',
+            store,
             posts: [],
             loading: true,
             currentPage: 1,
@@ -28,7 +33,7 @@ export default {
             //    }
             //});
 
-            axios.get(`${this.baseUrl}/api/posts`, { params: { page: numPage } })
+            axios.get(`${this.store.baseUrl}/api/posts`, { params: { page: numPage } })
                 .then((response) => {
                     if (response.data.success) {
                         this.posts = response.data.results.data;
@@ -48,33 +53,23 @@ export default {
 
 <template>
     <div>
-        <div v-for="post in posts" :key="post.id" class="post-card">
-            <div class="card">
-                <h2>{{ post.title }}</h2>
-                <p>{{ post.content }}</p>
-                <div v-if="post.tags">
-                    <div v-for="tag in post.tags" :key="tag.id">
-                        <h1>{{ tag.name }}</h1>
-                    </div>
-                </div>
-                <div>
-                    <span v-if="post.category">{{ post.category.name }}</span>
-                    <span v-else>Categoria non assegnata</span>
-                </div>
-            </div>
-        </div>
-        <div v-if="loading" class="loading">Loading...</div>
         <div>
-            <nav>
-                <ul>
-                    <li :class="currentPage === 1 ? 'disabled' : ''">
-                        <button @click="getPosts(currentPage - 1)">Precedente</button>
-                    </li>
-                    <li :class="currentPage === lastPage ? 'disabled' : ''">
-                        <button @click="getPosts(currentPage + 1)">Successiva</button>
-                    </li>
-                </ul>
-            </nav>
+            <div v-for="post in posts" :key="post.id" class="post-card">
+                <Postcard :post="post"></Postcard>
+            </div>
+            <div v-if="loading" class="loading">Loading...</div>
+            <div>
+                <nav>
+                    <ul>
+                        <li :class="currentPage === 1 ? 'disabled' : ''">
+                            <button @click="getPosts(currentPage - 1)">Precedente</button>
+                        </li>
+                        <li :class="currentPage === lastPage ? 'disabled' : ''">
+                            <button @click="getPosts(currentPage + 1)">Successiva</button>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </div>
     </div>
 </template>
@@ -84,19 +79,5 @@ export default {
 .post-card {
     display: inline-block;
     margin: 10px;
-}
-
-.card {
-    border: 1px solid #ccc;
-    padding: 10px;
-}
-
-.card h2 {
-    margin: 0;
-}
-
-.loading {
-    text-align: center;
-    padding: 20px;
 }
 </style>
